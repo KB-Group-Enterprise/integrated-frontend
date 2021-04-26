7c8239deb6dba300b1e6728356d84fbf86d30ded
KlitB <sapondanai.kb@mail.kmutt.ac.th>
2021-04-26 19:52:36 +0700

    [Update] Card Components

---
+++ src/components/Card.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+  <div>
+    <div class="grid justify-items-center">
+      <div class="relative bg-white py-6 px-6 w-80 my-10 shadow-xl">
+        <div>
+          <div>
+            <img
+              class="w-full object-cover"
+              src="https://www.mercedes-benz.co.uk/passengercars/mercedes-benz-cars/models/cls/coupe-c257/amg/model-lines/_jcr_content/swipeableteaserbox/par/swipeableteaser_185438073/asset.MQ6.12.20191106114837.jpeg"
+            />
+          </div>
+          <p class="text-sm font-semibold mt-2">Mercedes</p>
+          <p class="text-xl font-bold">RANDOMSHIT-2050</p>
+          <p class="text-sm font-semibold mb-2 text-gray-500">6,900,000.-</p>
+          <div class="border-t-2 pb-10"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Card',
+};
+</script>
+<style lang=""></style>
