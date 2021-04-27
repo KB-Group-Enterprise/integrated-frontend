a3936132af104b1146387786702b65389afecddd
Prakit Wongnuntananon <boatnakrub_@hotmail.com>
2021-04-27 22:26:56 +0700

    [Update] environment variable for deploy

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM node:14.16.1-alpine3.10 as build
 COPY . /myfrontend
 WORKDIR /myfrontend
 RUN npm install
+ARG BACKEND_URL
+ENV VUE_APP_BACKEND_URL $BACKEND_URL
 RUN npm run build
 
 FROM nginx:1.19.10-alpine
--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 let baseURL =
   process.env.NODE_ENV == 'production'
-    ? process.env.BACKEND_URL
+    ? process.env.VUE_APP_BACKEND_URL
     : 'http://localhost:5000';
 export default axios.create({
   baseURL,
