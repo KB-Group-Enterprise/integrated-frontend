796951bcbe60821eb5e664469248a56589c3def5
BoatPrakit <boatnakrub_@hotmail.com>
2021-04-27 16:15:38 +0700

    [Add] Brands, CardContainer, Dropdown

---
+++ src/components/Brands.vue
@@ -0,0 +1,35 @@
+<template>
+  <Dropdown @selected-brand="setSelectedBrand"></Dropdown>
+  <CardContainer :cars="carsInBrand"></CardContainer>
+</template>
+
+<script>
+import Dropdown from '@/components/Dropdown.vue';
+import CardContainer from '@/components/CardContainer.vue';
+export default {
+  name: 'brands',
+  components: {
+    Dropdown,
+    CardContainer,
+  },
+  data() {
+    return {
+      brandName: '',
+      carsInBrand: [],
+    };
+  },
+  methods: {
+    setSelectedBrand({ brandName }) {
+      this.brandName = brandName;
+    },
+  },
+  watch: {
+    async brandName() {
+      // const res = await this.getHttp(`/api/brands/${this.brandName}/cars`);
+      const res = await this.getHttp('/api/cars');
+      console.log(res);
+      this.carsInBrand = res.data;
+    },
+  },
+};
+</script>
---
+++ src/components/CardContainer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex flex-col md:flex-row">
+    <Card v-for="car in cars" :key="car.id" :car="car"></Card>
+  </div>
+</template>
+
+<script>
+import Card from '@/components/Card.vue';
+export default {
+  name: 'card-container',
+  props: ['cars'],
+  components: {
+    Card,
+  },
+};
+</script>
