9acff5d48246d9d99ed076317b08e6db56621908
KlitB <sapondanai.kb@mail.kmutt.ac.th>
2021-05-02 23:14:47 +0700

    [Reverse] Axios

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-let baseURL = 'http://13.94.1.42:5000';
+let baseURL =
+  process.env.NODE_ENV == 'production'
+    ? process.env.VUE_APP_BACKEND_URL
+    : 'http://localhost:5000';
 export default axios.create({
   baseURL,
 });
