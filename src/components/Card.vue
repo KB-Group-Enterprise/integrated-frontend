849c698a93d66a87f56b4b020eeb988765fd98db
BoatPrakit <boatnakrub_@hotmail.com>
2021-05-05 14:43:43 +0700

    [Update] loading animation

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,13 +2,31 @@
   <div class="relative bg-white py-6 px-6 hover:bg-gray-100">
     <div>
       <div>
-        <img class="h-32 md:h-40 xl:h-60 w-full object-cover" :src="imageUrl" />
+        <div
+          class="h-32 md:h-40 xl:h-60 bg-gray-300 animate-pulse"
+          v-if="!isImageLoaded"
+        ></div>
+        <img
+          class="h-32 md:h-40 xl:h-60 w-full object-cover"
+          :src="imageUrl"
+          @load="isImageLoaded = true"
+          v-show="isImageLoaded"
+        />
       </div>
-      <p class="text-sm font-semibold mt-2">{{ car.brand.name }}</p>
-      <p class="text-xl font-bold">{{ car.name }}</p>
-      <p class="text-sm font-semibold mb-2 text-gray-500">
-        {{ priceDetection }}.-
-      </p>
+      <div>
+        <div class="animate-pulse" v-if="!isImageLoaded">
+          <div class="rounded-full my-2 bg-gray-300 h-3 w-2/6"></div>
+          <div class="rounded-full bg-gray-300 h-3 w-5/6"></div>
+          <div class="rounded-full my-2 bg-gray-300 h-3 w-2/6"></div>
+        </div>
+        <div v-else>
+          <p class="text-sm font-semibold mt-2">{{ car.brand.name }}</p>
+          <p class="text-xl font-bold">{{ car.name }}</p>
+          <p class="text-sm font-semibold mb-2 text-gray-500">
+            {{ priceDetection }}.-
+          </p>
+        </div>
+      </div>
       <div class="border-t-2 pb-10"></div>
     </div>
   </div>
@@ -17,6 +35,11 @@
 export default {
   name: 'Card',
   props: ['car'],
+  data() {
+    return {
+      isImageLoaded: false,
+    };
+  },
   computed: {
     priceDetection() {
       var formatter = new Intl.NumberFormat('en-US', {
--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,11 +1,30 @@
-<template lang=""><div> <Brands></Brands> </div></template>
+<template>
+  <div class="flex w-full">
+    <button class="bg-white" @click="componentName = 'add'">Add +</button>
+  </div>
+  <keep-alive>
+    <component :is="currentComponent"></component>
+  </keep-alive>
+</template>
+
 <script>
 import Brands from '@/components/Brands';
+import Add from '@/views/Add.vue';
 export default {
   name: 'Products',
   components: {
     Brands,
+    Add,
+  },
+  data() {
+    return {
+      componentName: 'brands',
+    };
+  },
+  computed: {
+    currentComponent() {
+      return this.componentName;
+    },
   },
 };
 </script>
-<style lang=""></style>
