33ff505903ed3abcfd2675a0bf106516e735be6d
KlitB <sapondanai.kb@mail.kmutt.ac.th>
2021-05-05 10:50:16 +0700

    [Update] rename Product to Products

---
+++ src/components/Products.vue
@@ -0,0 +1,11 @@
+<template lang=""><div> <Brands></Brands> </div></template>
+<script>
+import Brands from '@/components/Brands';
+export default {
+  name: 'Products',
+  components: {
+    Brands,
+  },
+};
+</script>
+<style lang=""></style>
